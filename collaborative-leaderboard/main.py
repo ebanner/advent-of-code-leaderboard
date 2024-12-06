e4d9543685fd4f4c9c7978753c53a73ffa62acf2
Edward Banner <edward.banner@gmail.com>
2024-12-05 20:19:22 -0500

    Change silver star emoji

--- collaborative-leaderboard/main.py
+++ collaborative-leaderboard/main.py
@@ -94,7 +94,7 @@ def get_grid(stars, members):
 
         num_silver = stars[str(day)]['silver']
         for i in range(num_silver):
-            grid[num_gold+i][j] = ' ★'
+            grid[num_gold+i][j] = '🥈'
 
     return grid
 
